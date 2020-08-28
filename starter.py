cb5e1aa8c9e9183370a1f379a8149d80061d543c
Mysta3 <myshawnestallings@gmail.com>
2020-08-28 16:01:59 -0700

    added generating ID function to starter file

--- starter.py
+++ starter.py
@@ -1,105 +1,139 @@
 # # Unlock Academy
 
-# # Homework 1 & 2
-
-# # initialize empty dict
-# # user_profile = {'name': '', 'dob': '', 'years_of_experience': '',
-# #                 'num_of_languages': '', 'salary_range': '', 'is_active': ''}
+# Homework 1 & 2
 
 
-# # # assign user input to dict key/pairs
-# # user_profile['name'] = input('Enter your Name: ')
-# # user_profile['dob'] = input('What is your Date of Birth: ')
+def printDivider(name):
+    print(f'----------{name.upper()}---------------')
 
 
-# # user_coding_languages = input("List coding languages: ")
-# # user_languages = user_coding_languages.strip().split(',')
-# # user_answer = input('Are you actively looking? Respond Y/N: ')
-# # user_profile['is_active'] = False
-# # if(user_answer.lower() == 'y'):
-# #     user_profile['is_active'] = True
-# # elif(user_answer.lower() == 'n'):
-# #     user_profile['is_active'] = False
-# # else:
-# #     print('[-] ERROR, please re-enter using y/n as an answer')
+# initialize empty dict
+user_profile = {'name': '', 'dob': '', 'years_of_experience': '',
+                'num_of_languages': '', 'salary_range': '', 'is_active': ''}
 
 
-# # user_profile['num_of_languages'] = len(user_languages)
+# assign user input to dict key/pairs
+user_profile['name'] = input('Enter your Name: ')
+user_profile['dob'] = input('What is your Date of Birth: ')
 
 
-# # user_experience = input(
-# #     'How many years of development experience do you have? \n [1] < 1 year \n [2] 1 - 3 years \n [3] 3 - 8 years \n [4] 8+ years \n Enter Here:  ')
-
-# # user_experience = int(user_experience)
-# # if user_experience == 1:
-# #     user_profile['years_of_experience'] = '< 1 year'
-# #     min_amount = 30
-# #     max_amount = 60
-# #     if len(user_languages) < 2:
-# #         amount = min_amount - 10
-# #         amount_max = max_amount - 10
-# #         user_profile['salary_range'] = (
-# #             f'{amount}k - {amount_max}k')
-# #     print(
-# #         f'Entry Level Salary ranges from {amount}k - {amount_max}k annually')
-
-# # elif user_experience == 2:
-# #     user_profile['years_of_experience'] = '1 - 3 years'
-# #     min_amount = 60
-# #     max_amount = 80
-# #     if len(user_languages) < 3:
-# #         amount = min_amount - 10
-# #         amount_max = max_amount - 10
-# #         user_profile['salary_range'] = (
-# #             f'{amount}k - {amount_max}k')
-# #     print(
-# #         f"Junior Level Salary ranges from {amount}k - {amount_max}k")
-# # elif user_experience == 3:
-# #     user_profile['years_of_experience'] = '3 - 8 years'
-# #     min_amount = 80
-# #     max_amount = 90
-# #     if len(user_languages) < 4:
-# #         amount = min_amount - 10
-# #         amount_max = max_amount - 10
-# #         user_profile['salary_range'] = (
-# #             f'{amount}k - {amount_max}k')
-# #     print(
-# #         f"Mid-Level Salary ranges from {amount_max}k - {amount_max}k")
-# # elif user_experience == 4:
-# #     user_profile['years_of_experience'] = '8+ years'
-# #     user_profile['salary_range'] = '100k'
-# #     print('WELCOME TO THE PROMISE LAND OF CODE!!! SENIOR LEVEL BABY!! 100k+')
-# # else:
-# #     print('[-] Please pick a valid value')
-# #     user_experience = input(
-# #         'How many years of development experience do you have? \n [1] < 1 year \n [2] 1 - 3 years \n [3] 3 - 8 years \n [4] 8+ years \n Enter Here:  ')
+user_coding_languages = input("List coding languages: ")
+user_languages = user_coding_languages.strip().split(',')
+user_answer = input('Are you actively looking? Respond Y/N: ')
+user_profile['is_active'] = False
+if(user_answer.lower() == 'y'):
+    user_profile['is_active'] = True
+elif(user_answer.lower() == 'n'):
+    user_profile['is_active'] = False
+else:
+    print('[-] ERROR, please re-enter using y/n as an answer')
 
 
-# # print(user_profile)
+user_profile['num_of_languages'] = len(user_languages)
 
-# # WHILE LOOP
-# print('-------WHILE LOOP -------')
+
+user_experience = input(
+    'How many years of development experience do you have? \n [1] < 1 year \n [2] 1 - 3 years \n [3] 3 - 8 years \n [4] 8+ years \n Enter Here:  ')
+
+user_experience = int(user_experience)
+if user_experience > 4:
+    raise ValueError
+
+elif user_experience == 1:
+    user_profile['years_of_experience'] = '< 1 year'
+    min_amount = 30
+    max_amount = 60
+    if len(user_languages) < 2:
+        amount = min_amount - 10
+        amount_max = max_amount - 10
+        user_profile['salary_range'] = (
+            f'{amount}k - {amount_max}k')
+    else:
+        amount = min_amount
+        amount_max = max_amount
+    user_profile['salary_range'] = (
+        f'{amount}k - {amount_max}k')
+    print(
+        f'Entry Level Salary ranges from {min_amount}k - {max_amount}k annually')
+
+elif user_experience == 2:
+    user_profile['years_of_experience'] = '1 - 3 years'
+    min_amount = 60
+    max_amount = 80
+    if len(user_languages) < 3:
+        amount = min_amount - 10
+        amount_max = max_amount - 10
+        user_profile['salary_range'] = (
+            f'{amount}k - {amount_max}k')
+    else:
+        amount = min_amount
+        amount_max = max_amount
+        user_profile['salary_range'] = (
+            f'{amount}k - {amount_max}k')
+    print(
+        f"Junior Level Salary ranges from {amount}k - {amount_max}k")
+
+elif user_experience == 3:
+    user_profile['years_of_experience'] = '3 - 8 years'
+    min_amount = 80
+    max_amount = 90
+    if len(user_languages) < 4:
+        amount = min_amount - 10
+        amount_max = max_amount - 10
+        user_profile['salary_range'] = (
+            f'{amount}k - {amount_max}k')
+    else:
+        amount = min_amount
+        amount_max = max_amount
+        user_profile['salary_range'] = (
+            f'{amount}k - {amount_max}k')
+    print(
+        f"Mid-Level Salary ranges from {amount_max}k - {amount_max}k")
+elif user_experience == 4:
+    user_profile['years_of_experience'] = '8+ years'
+    user_profile['salary_range'] = '100k'
+    print('WELCOME TO THE PROMISE LAND OF CODE!!! SENIOR LEVEL BABY!! 100k+')
+else:
+    print('[-] Please pick a valid value')
+    user_experience = input(
+        'How many years of development experience do you have? \n [1] < 1 year \n [2] 1 - 3 years \n [3] 3 - 8 years \n [4] 8+ years \n Enter Here:  ')
+
+# Homework 5
+
+
+def create_user_id():
+    user_id = id(user_profile['name'])
+    user_profile.update({'id': user_id})
+    printDivider('UNIQUE ID')
+
+
+create_user_id()
+
+print(user_profile)
+
+# WHILE LOOP
+# printDivider('while loops')
 # list_of_colors = ['red', 'yellow', 'blue']
 # index = 0
 # while len(list_of_colors) > index:
 #     print(list_of_colors[index])
 #     index += 1
 
-# print('-------FOR LOOP -------')
+# printDivider('for loops')
 # for item in list_of_colors:
 #     print('[x]', item)
 
-# print('--- PRINT 0 to 10 ----')
+# printDivider('range function')
 # x = range(0, 11)  # print 0 - 10
 # for num in x:
 #     print(num)
 
-# print('---- PRINT EVERY 2 ----')
+# printDivider('range with a step of 2')
 # x = range(0, 11, 2)
 # for num in x:
 #     print(num)
 
-# print('<---- PRINT ODDS ONLY --->')
+# printDivider('print odd numbers')
 # x = range(0, 11)
 # for num in x:
 #     if (num % 2 != 0):
@@ -107,14 +141,10 @@
 
 
 # # Looping over Dictionaries
-# print('------ LOOP OVER DICTIONARY')
+# printDivider('loop over dictionaries')
 # dictionary_list = {'1': 100, '2': 200, '3': 300}
 # print(dictionary_list.keys())
 # print(dictionary_list.values())
 
 # for key in dictionary_list:
 #     print(key, ':', dictionary_list[key])
-
-
-
-
